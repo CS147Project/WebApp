33d9e380a7ef20e7fa1afa76498d62ff9ed41872
Jeremy Hines <jchines@stanford.edu>
2014-02-20 01:44:38 -0800

    Set up initial mongoose/mongo tables.

--- app.js
+++ app.js
@@ -7,91 +7,8 @@ var express = require('express');
 var http = require('http');
 var path = require('path');
 var handlebars = require('express3-handlebars');
-var mysql = require('mysql');
+var mongoose = require('mongoose');
 
-//////// MYSQL CONFIG CODE
-var connection = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : '', 
-});
-
-
-
-connection.query('CREATE DATABASE IF NOT EXISTS appdb');
-connection.query('USE appdb');
-
-// Create db tables for app. Add new tables below.
-// User table
-connection.query("CREATE TABLE IF NOT EXISTS users (" +
-  "email varchar(40) NOT NULL," +
-  "firstName varchar(25) NOT NULL," +
-  "lastName varchar(25) NOT NULL," +
-  "nickname varchar(25)," +
-  "password varchar(40) NOT NULL," +
-  "PRIMARY KEY (email)" +
-  ")");
-
-// Athlete table
-connection.query("CREATE TABLE IF NOT EXISTS athletes (" +
-  "aid varchar(40) NOT NULL," +
-  "PRIMARY KEY (aid)," +
-  "FOREIGN KEY (aid) REFERENCES users(email)"+
-  ")");
-
-// Coach table
-connection.query("CREATE TABLE IF NOT EXISTS coaches (" +
-  "cid varchar(40) NOT NULL," +
-  "PRIMARY KEY (cid)," +
-  "FOREIGN KEY (cid) REFERENCES users(email)"+
-  ")");
-
-// Invite table
-connection.query("CREATE TABLE IF NOT EXISTS invite (" +
-  "iid bigint NOT NULL AUTO_INCREMENT," +
-  "aid varchar(25) NOT NULL," +
-  "tid bigint NOT NULL," +
-  "created timestamp default now()," +
-  "PRIMARY KEY (iid)," +
-  "FOREIGN KEY (aid) REFERENCES athletes(aid),"+
-  "FOREIGN KEY (tid) REFERENCES teams(tid)"+
-  ")");
-
-// Message table
-connection.query("CREATE TABLE IF NOT EXISTS messages (" +
-  "mid bigint NOT NULL AUTO_INCREMENT," +
-  "text text NOT NULL," +
-  "fromid varchar(40) NOT NULL," +
-  "toid varchar(40) NOT NULL," +
-  "created timestamp default now()," +
-  "PRIMARY KEY (mid)," +
-  "FOREIGN KEY (fromid) REFERENCES users(email),"+
-  "FOREIGN KEY (toid) REFERENCES users(email)"+
-  ")");
-
-// Team table
-connection.query("CREATE TABLE IF NOT EXISTS teams (" +
-  "tid bigint NOT NULL AUTO_INCREMENT," +
-  "name varchar(45) NOT NULL," +
-  "sport varchar(45) NOT NULL," +
-  "PRIMARY KEY (tid)" +
-  ")");
-
-// TeamAthlete table
-connection.query("CREATE TABLE IF NOT EXISTS teamathletes (" +
-  "tid bigint NOT NULL," +
-  "aid varchar(40) NOT NULL," +
-  "FOREIGN KEY (tid) REFERENCES teams(tid),"+
-  "FOREIGN KEY (aid) REFERENCES athletes(aid)"+
-  ")");
-
-// TeamCoach table
-connection.query("CREATE TABLE IF NOT EXISTS teamcoaches (" +
-  "tid bigint NOT NULL," +
-  "cid varchar(40) NOT NULL," +
-  "FOREIGN KEY (tid) REFERENCES teams(tid),"+
-  "FOREIGN KEY (cid) REFERENCES athletes(cid)"+
-  ")");
 
 var admin = require('./routes/admin');
 var home = require('./routes/home');
@@ -99,6 +16,14 @@ var team = require('./routes/team');
 var messages = require('./routes/messages');
 var workout = require('./routes/workout');
 var workouts = require('./routes/workouts');
+
+
+// Connect to the Mongo database, whether locally or on Heroku
+var local_database_name = 'app';
+var local_database_uri  = 'mongodb://localhost/' + local_database_name
+var database_uri = process.env.MONGOLAB_URI || local_database_uri
+mongoose.connect(database_uri);
+
 
 var app = express();
 
